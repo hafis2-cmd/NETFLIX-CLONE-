3238289d54dc6e8daee6cf19d9e4a8ef148fe5b6
hafis2-cmd <hafismohammed73@gmail.com>
2025-07-03 21:05:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// navbar CSS property on scroll 
+window.addEventListener('scrolled',()=>{
+    const navbar=document.querySelector(".navbar");
+    if(window.scrollY>100){
+        navbar.classList.add(scrolled);
+    }else{
+        navbar.classList.remove("scrolled");
+    }
+});
+
+// profile section redirecting
+const profileIcon=document.querySelector(".profile-icon");
+profileIcon.addEventListener("click", ()=>{
+    window.location.href="profile.html";
+});
+
+// movie card multiple movie card so querySelectorAll
+const movieCard=document.querySelectorAll(".content-card");
+movieCard.forEach((card)=>{
+    card.addEventListener("mouseEnter",()=>{
+        card.computedStyleMap.transform="scale(1.05)";
+    });
+    card.addEventListener("mouseLeave",()=>{
+        card.computedStyleMap.transform="scale(1)";
+    });
+});
+
